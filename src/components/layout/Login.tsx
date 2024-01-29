bcb375464a1c67591709d2bd17fe5a33b70dc1d0
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-01-29 21:44:45 +0100

    jwt decode implimented to login

--- src/components/layout/Login.tsx
+++ src/components/layout/Login.tsx
@@ -1,11 +1,58 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../../redux/features/auth/authApi";
+import { useAppDispatch } from "../../redux/hooks";
+import { setUser } from "../../redux/features/auth/authSlice";
+import { verifyToken } from "../../utils/verifyToken";
+
 const Login = () => {
+  const { handleSubmit, register } = useForm({
+    defaultValues: {
+      username: "ashiq",
+      password: "123456",
+    },
+  });
+  const dispatch = useAppDispatch();
+  const [login, { error }] = useLoginMutation();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onsubmit = async (data: any) => {
+    const userInfo = {
+      username: data.username,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const userFromToken = verifyToken(res.data.token);
+    dispatch(setUser({ user: { user: userFromToken }, token: res.data.token }));
+  };
   return (
-    <div>
-      <label htmlFor="username">User Name:</label>
-      <input type="text" id="username" />
-      <label htmlFor="password">User Name:</label>
-      <input type="text" id="password" />
-    </div>
+    <form
+      onSubmit={handleSubmit(onsubmit)}
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "10px",
+      }}
+    >
+      <div>
+        <label htmlFor="username">User Name:</label>
+        <input type="text" id="username" {...register("username")} />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          id="password"
+          {...register("password")}
+          style={{ marginLeft: "12px" }}
+        />
+      </div>
+      <Button htmlType="submit">Submit</Button>
+    </form>
   );
 };
 
