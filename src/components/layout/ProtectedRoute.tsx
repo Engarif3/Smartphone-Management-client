b1154342f3e7619d97dd43739deb641895a21d0b
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-01-30 03:44:59 +0100

    after login redirect to expected page done by using useLocation and useNavigate on login  component

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,20 +1,13 @@
 import { ReactNode } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { useCurrentToken } from "../../redux/features/auth/authSlice";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-  const location = useLocation();
   const token = useAppSelector(useCurrentToken);
 
   if (!token) {
-    return (
-      <Navigate
-        to="/login"
-        state={{ from: location }}
-        replace={true}
-      ></Navigate>
-    );
+    return <Navigate to="/login" replace={true}></Navigate>;
   }
 
   return children;
