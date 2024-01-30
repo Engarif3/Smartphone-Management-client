4f4e6561806c2134645d922199378e628b7dc6ad
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-01-30 03:25:14 +0100

    protected route added

---
+++ src/components/CheckProtected.tsx
@@ -0,0 +1,9 @@
+const CheckProtected = () => {
+  return (
+    <div>
+      <h2>This is protected Route</h2>
+    </div>
+  );
+};
+
+export default CheckProtected;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../components/layout/Login";
+import CheckProtected from "../components/CheckProtected";
+import ProtectedRoute from "../components/layout/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: (
+      <ProtectedRoute>
+        <App />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/login",
     element: <Login></Login>,
+  },
+  {
+    path: "/protected",
+    element: (
+      <ProtectedRoute>
+        <CheckProtected />
+      </ProtectedRoute>
+    ),
   },
 ]);
 
